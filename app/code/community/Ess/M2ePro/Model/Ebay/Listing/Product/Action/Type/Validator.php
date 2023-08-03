985f76099869192a223b7866ee63256323060ff5
M2E Pro <m2epro@users.noreply.github.com>
2023-08-03 11:56:15 +0000

    6.43.5 (FINAL RELEASE)

--- app/code/community/Ess/M2ePro/Model/Ebay/Listing/Product/Action/Type/Validator.php
+++ app/code/community/Ess/M2ePro/Model/Ebay/Listing/Product/Action/Type/Validator.php
@@ -346,15 +346,13 @@ abstract class Ess_M2ePro_Model_Ebay_Listing_Product_Action_Type_Validator
         /** @var Ess_M2ePro_Model_Resource_Listing_Product_Variation_Collection $collection */
         $collection = $listingProduct->getResourceCollection();
         $collection->getSelect()->reset(\Zend_Db_Select::COLUMNS);
-        $collection->getSelect()->columns(array(
-            'count_deleted' => new \Zend_Db_Expr('SUM(IF(second_table.`delete`, 1, 0))'),
-        ));
         $collection->getSelect()->joinLeft(
             array('vo' => $variationOptionResource->getMainTable()),
             'vo.listing_product_variation_id = main_table.id',
             array(
                 'attribute_name' => 'vo.attribute',
-                'count_options' => new \Zend_Db_Expr('COUNT(DISTINCT IF(second_table.`delete`, NULL, vo.`option`))')
+                'count_variation_options'
+                    => new \Zend_Db_Expr('COUNT(DISTINCT IF(second_table.`delete`, NULL, vo.`option`))')
             )
         );
         $collection->getSelect()->group('vo.attribute');
@@ -377,14 +375,33 @@ abstract class Ess_M2ePro_Model_Ebay_Listing_Product_Action_Type_Validator
             return false;
         }
 
-        $totalVariationsCount = 1;
-        $totalDeletedVariationsCount = 0;
+        // All options deleted
+        $totalVariationOptions = 0;
         foreach ($data as $item) {
-            $totalVariationsCount *= $item['count_options'];
-            $totalDeletedVariationsCount += $item['count_deleted'];
+            $totalVariationOptions += $item['count_variation_options'];
+        }
+
+        if ($totalVariationOptions === 0) {
+            $this->addMessage(
+                'This Product was listed to eBay as Variational Item.
+                Changing of the Item type from Variational to Non-Variational during Revise/Relist
+                actions is restricted by eBay.
+                At the moment this Product is considered as Simple without any Variations,
+                that does not allow updating eBay Variational Item.'
+            );
+            return false;
+        }
+
+        $totalOptionsCount = 1;
+        foreach ($data as $item) {
+            if ((int)$item['count_variation_options'] === 0) {
+                continue;
+            }
+
+            $totalOptionsCount *= $item['count_variation_options'];
             // Maximum 60 options by one attribute:
             // Color: Red, Blue, Green, ...
-            if ($item['count_options'] > 60) {
+            if ($item['count_variation_options'] > 60) {
                 $this->addMessage(
                     'Variations of this Magento Product are out of the eBay Variational Item limits.
                         Its number of Options for some Variational Attribute(s) is more than 60.
@@ -394,8 +411,8 @@ abstract class Ess_M2ePro_Model_Ebay_Listing_Product_Action_Type_Validator
                 return false;
             }
 
-            // Not more that 250 possible variations
-            if ($totalVariationsCount > 250) {
+            // Not more than 250 possible variations
+            if ($totalOptionsCount > 250) {
                 $this->addMessage(
                     'Variations of this Magento Product are out of the eBay Variational Item limits.
                     The Number of Variations is more than 250. That is why, this Product cannot be updated on eBay.
@@ -403,17 +420,6 @@ abstract class Ess_M2ePro_Model_Ebay_Listing_Product_Action_Type_Validator
                 );
                 return false;
             }
-        }
-
-        if ($totalVariationsCount == $totalDeletedVariationsCount) {
-            $this->addMessage(
-                'This Product was listed to eBay as Variational Item.
-                Changing of the Item type from Variational to Non-Variational during Revise/Relist
-                actions is restricted by eBay.
-                At the moment this Product is considered as Simple without any Variations,
-                that does not allow updating eBay Variational Item.'
-            );
-            return false;
         }
 
         return true;
